fab6f837b15c5d9ba7d28e0d0caf37606da86394
Alan-Shibu <alan.shibu@jobinandjismi.com>
2025-06-12 14:10:57 +0530

    OTP-8981 -ci : creating a script to generate custom blood donor records  based on form data

---
+++ OTP-8981/OTP-8981/src/FileCabinet/SuiteScripts/JobinAndJismi/OTP-8981/jj_sl_otp_8981.js
@@ -0,0 +1,204 @@
+/**
+ * @NApiVersion 2.1
+ * @NScriptType Suitelet
+ * @NModuleScope SameAccount
+ */
+/*************************************************************************************
+ *
+ *
+ * ${OTP-8981} : ${Custom form to store blood donor details and track them in database}
+ *
+ *
+ **************************************************************************************
+ *
+ * Author: Jobin and Jismi IT Services
+ *
+ * Date Created : 09-June-2025
+ *
+ * Description : This script is for creating a custom form, which accepts the details of
+ * potential blood donors with fields for entering the donor name,(first name,last name),
+ * gender, phone number,blood group and last donation date.A custom record type should
+ * be created in NetSuite to store the data received through the form.
+ *
+ * REVISION HISTORY
+ *
+ * @version 1.0   : 09-June-2025 : The initial build was created by JJ0401
+ *
+ *
+ *************************************************************************************
+ **********/
+define(["N/log", "N/record", "N/ui/serverWidget"], 
+ /**
+ * @param{log} log
+ * @param{record} record
+ * @param{serverWidget} serverWidget
+ */
+(log, record, serverWidget) => {
+  /**
+   * Defines the Suitelet script trigger point.
+   * @param {Object} scriptContext
+   * @param {ServerRequest} scriptContext.request - Incoming request
+   * @param {ServerResponse} scriptContext.response - Suitelet response
+   * @since 2015.2
+   */
+  const onRequest = (scriptContext) => {
+    try {
+      if (scriptContext.request.method === "GET") {
+        formCreation(scriptContext);
+      }
+      if (scriptContext.request.method === "POST") {
+        recordCreation(scriptContext);
+      }
+    } catch (e) {
+      log.error("Error caught", e.message);
+    }
+
+    /**
+     * Function to create a custom form to enter the blood donor details
+     * @param
+     * @returns {void}
+     */
+    function formCreation() {
+      try {
+        let form = serverWidget.createForm({
+          title: "Blood Requirement Registration Form",
+        });
+
+        let fstNameField = form.addField({
+          id: "custpage_fst_name",
+          type: serverWidget.FieldType.TEXT,
+          label: "First Name",
+        });
+
+        let lastNameField = form.addField({
+          id: "custpage_lst_name",
+          type: serverWidget.FieldType.TEXT,
+          label: "Last Name",
+        });
+
+        let genderField = form.addField({
+          id: "custpage_gender",
+          type: serverWidget.FieldType.SELECT,
+          label: "Gender",
+          source: "customlist_jj_gender",
+        });
+
+        let phoneNoField = form.addField({
+          id: "custpage_phn_no",
+          type: serverWidget.FieldType.PHONE,
+          label: "Phone Number",
+        });
+
+        let bloodGrpField = form.addField({
+          id: "custpage_bld_grp",
+          type: serverWidget.FieldType.SELECT,
+          label: "Blood Group",
+          source: "customlist_jj_blood_grp",
+        });
+
+        let lastDonationDayField = form.addField({
+          id: "custpage_last_donation",
+          type: serverWidget.FieldType.DATE,
+          label: "Last Donation Date",
+        });
+
+        form.addSubmitButton({
+          label: "Submit Donor data",
+        });
+
+        scriptContext.response.writePage({
+          pageObject: form,
+        });
+      } catch (e) {
+        log.error("Error caught", e.message);
+      }
+    }
+
+    /**
+     * Function to create a custom record with the data received through the form
+     * @param
+     * @returns {void}
+     */
+    function recordCreation(scriptContext) {
+      try {
+        let firstName = scriptContext.request.parameters.custpage_fst_name;
+
+        let lastName = scriptContext.request.parameters.custpage_lst_name;
+
+        let gender = scriptContext.request.parameters.custpage_gender;
+
+        let phoneNumber = scriptContext.request.parameters.custpage_phn_no;
+
+        let bloodGroup = scriptContext.request.parameters.custpage_bld_grp;
+
+        let lastDonationDay =
+          scriptContext.request.parameters.custpage_last_donation;
+
+        let newDate = convertDate(lastDonationDay);
+
+        let bloodRequirementRecord = record.create({
+          type: "customrecord_jj_blood_requirement",
+          isDynamic: true,
+        });
+
+        bloodRequirementRecord.setValue({
+          fieldId: "custrecord_jj_fst_name",
+          value: firstName,
+        });
+        bloodRequirementRecord.setValue({
+          fieldId: "custrecord_jj_lst_name",
+          value: lastName,
+        });
+        bloodRequirementRecord.setValue({
+          fieldId: "custrecord_jj_blood_donor_gender",
+          value: gender,
+        });
+        bloodRequirementRecord.setValue({
+          fieldId: "custrecord_jj_donor_phn_no",
+          value: phoneNumber,
+        });
+        bloodRequirementRecord.setValue({
+          fieldId: "custrecord_jj_bld_grp",
+          value: bloodGroup,
+        });
+        bloodRequirementRecord.setValue({
+          fieldId: "custrecord_jj_last_bld_donation_date",
+          value: newDate,
+        });
+
+        bloodRequirementRecord.save({
+          ignoreMandatoryFields: true,
+        });
+
+        log.debug("Record ");
+      } catch (e) {
+        log.error("Error caught", e.message);
+      }
+    }
+
+    /**
+     * Function to convert a date into standard date format of NetSuite
+     * @param {date} - the date that needs to be converted
+     * @returns {dateObj} - the formatted date
+     */
+    function convertDate(date) {
+      try {
+        let dateObj = new Date(date);
+        let month = dateObj.getMonth() + 1;
+        let day = dateObj.getDate(); // Correct method for day of the month
+        let year = dateObj.getFullYear();
+
+        let newMonth = month < 10 ? "0" + month.toString() : month.toString();
+        let newDay = day < 10 ? "0" + day.toString() : day.toString();
+
+        let newdate = [year, newMonth, newDay].join("-"); // Correct format
+        let formattedDate = new Date(newdate);
+
+        return formattedDate;
+      } catch (e) {
+        log.error("Error caught", e.message);
+      }
+    }
+  };
+  return { onRequest };
+});
